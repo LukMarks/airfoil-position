e8cb8eda6715224c0e8413a1e2573bea0703d336
Lucas Marques Moreno <lucas.mmoreno@hotmail.com>
2020-08-16 18:33:49 -0300

    Initial commit

---
+++ src/airfoilPositioner.java
@@ -0,0 +1,22 @@
+/*==============================
+* Author: Lucas Marques Moreno
+* Date: 08/20
+* Description: A simple program
+* to rotate and resize an
+* airfoil form a .dat ou .csv
+* file.
+**==============================
+* */
+
+public class airfoilPositioner {
+
+    public static void main(String[] args){
+
+        System.out.println("===============================");
+        System.out.println(" Welcome to airfoil Positioner");
+        System.out.println("===============================");
+
+
+    }
+
+}
